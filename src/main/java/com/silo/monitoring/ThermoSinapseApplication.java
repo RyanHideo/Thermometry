c09c915939f2960c76022ba6f728d69d8189af68
Ryan <ryanhougaske@gmail.com>
2025-02-28 07:52:42 -0400

    first commit

---
+++ src/main/java/com/silo/monitoring/ThermoSinapseApplication.java
@@ -0,0 +1,13 @@
+package com.silo.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThermoSinapseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThermoSinapseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/silo/monitoring/ThermoSinapseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silo.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThermoSinapseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
